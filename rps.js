a5f1369e122e6e0e8afcad8019ef88eaa05bd128
Ries <25RiesC@badinhs.org>
2025-02-28 13:23:06 -0500

    finished steps

--- rps.js
+++ rps.js
@@ -1,1 +1,15 @@
-//gg
+console.log ("Welcome to Rock, Paper, Scissors")
+
+
+/* steps vvv
+- prompt for imput(rock, paper, or scissors)
+- if none of them, ask again
+- record imput
+- computer randomly comes up with an num, 
+- round to integer, assign integers to r, p, and s (1,2,3)
+- if imput =rock && generated num=paper, lose, console log (you lose, paper beats rock)
+- for all of them, add one to win counter, if loss, add one to loss counter
+- if draw, prompt again, dont record a win or loss
+- once either win or loss reaches 5, display "game win" or "game loss" accordingly.
+*/
+
